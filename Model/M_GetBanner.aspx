8387c2a0cf60abb28e3067c6b9aa086a8bb12a53
Ricky <ak47ricky24@gmail.com>
2018-04-19 16:28:07 +0800

    CCC2

---
+++ Model/M_GetBanner.aspx
@@ -0,0 +1,2 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="M_GetBanner.aspx.cs" Inherits="Model_M_GetBanner" %>
+
---
+++ Model/M_Test.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Model_M_Test : System.Web.UI.Page
+{
+    private string mExString;
+    private int mExNumber;
+
+    public string ExString { get { return mExString; } set { SetUseString(value); } }
+    public int ExNumber { get { return mExNumber; } set { SetExNumber(value); } }
+
+    private void SetUseString(string iString)//這邊以 i 為主。
+    {
+        mExString = iString;
+    }
+
+    private int SetExNumber(int iNumber)
+    {
+        int aTempNumber = 0;//區域變數 以 a 為主
+        if (mExNumber >= iNumber)
+            aTempNumber = mExNumber + iNumber;
+        else
+            aTempNumber = iNumber;
+
+        return aTempNumber;
+
+    }
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+    }
+
+
+
+}
