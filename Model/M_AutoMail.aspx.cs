3b710a5fc948debb6a8b1a79ba6883e07223d13d
Rockymania <rockymania@gmail.com>
2018-05-14 16:24:30 +0800

    Account

--- Model/M_AutoMail.aspx.cs
+++ Model/M_AutoMail.aspx.cs
@@ -13,22 +13,7 @@ public partial class Model_M_AutoMail : System.Web.UI.Page
     {
         try
         {
-            MailMessage message = new MailMessage();
-            message.From = new MailAddress("mobiledaddy@mobiledaddy.net");
 
-            message.To.Add(new MailAddress("ak47ricky24@gmail.com"));
-
-            message.Subject = "標題";
-            message.Body = "嗨起來";
-            message.IsBodyHtml = true;
-
-
-            SmtpClient SmtpServer = new SmtpClient();
-            SmtpServer.Credentials = new System.Net.NetworkCredential("mobiledaddy@mobiledaddy.net", "Aa54380438!");
-            SmtpServer.Port = 587;
-            SmtpServer.Host = "smtp.office365.com";
-            SmtpServer.EnableSsl = true;
-            SmtpServer.Send(message);
 
         }
         catch (Exception ex)
