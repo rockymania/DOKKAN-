787cd2f3e36a60edc3c57899ac8a6ae16bd0d820
Ricky <ak47ricky24@gmail.com>
2018-05-21 16:38:23 +0800

    先搶先贏

--- Backstage/Js/Button.js
+++ Backstage/Js/Button.js
@@ -9,6 +9,12 @@
     "<a href=\"V_LoginData.aspx\" class=\"easyui-linkbutton\" style=\"width:100%;height:50px;\">登入資料</a>" +
     "</div>" +
     "<div style=\"padding:5px 5px 5px 5px\">" +
-    "<a href=\"V_SaleData.aspx\" class=\"easyui-linkbutton\" style=\"width:100%;height:50px;\">單項商品銷售</a>" +
+    "<a href=\"#\" class=\"easyui-linkbutton\" style=\"width:100%;height:50px;\">單項商品銷售</a>" +
+    "</div>" +
+    "<div style=\"padding:5px 5px 5px 65px\">" +
+    "<a href=\"V_SaleData.aspx\" class=\"easyui-linkbutton\" style=\"width:80%;height:50px;\">銷售表格顯示</a>" +
+    "</div>" +
+    "<div style=\"padding:5px 5px 5px 65px\">" +
+    "<a href=\"V_SaleData.aspx\" class=\"easyui-linkbutton\" style=\"width:80%;height:50px;\">銷售圖表顯示</a>" +
     "</div>" 
 );
